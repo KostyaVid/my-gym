4cc9abce6ac183f51357e1bc04f75466494bd7a2
KostyaVid <constat@live.com>
2023-07-15 14:50:35 +0300

    add Programm in the ProgrammsHome

--- pages/ProgrammsHome/Programm/Programm.tsx
+++ pages/ProgrammsHome/Programm/Programm.tsx
@@ -1,4 +1,4 @@
-import { FlatList, StyleSheet } from "react-native";
+import { FlatList, ListRenderItem, StyleSheet, View } from "react-native";
 import React from "react";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { ProgrammStackList, RootStackParamList } from "../../../types";
@@ -6,20 +6,20 @@ import { RouteProp } from "@react-navigation/native";
 import globalStyle from "../../../utils/styles";
 import { useStore } from "../../../store/rootStore.store";
 import Container from "../../../components/Container/Container";
-import { Text } from "react-native-paper";
-import ExerciseThumb from "../../../components/ExerciseThumb/ExerciseThumb";
+import { Divider, Surface, Text, TouchableRipple } from "react-native-paper";
+import BackgroundImageSurface from "../../../components/BackgoundImageSurface/BackgroundImageSurface";
+import { TrainingDataProps } from "../../../data/programms";
+import { observer } from "mobx-react-lite";
 
-type TrainingScreenNavigationProp = NativeStackNavigationProp<
-  RootStackParamList,
-  "DailyHome"
->;
+type TrainingScreenNavigationProp =
+  NativeStackNavigationProp<RootStackParamList>;
 
 type Props = {
   navigation: TrainingScreenNavigationProp;
   route: RouteProp<ProgrammStackList, "Programm">;
 };
 
-const Programm = ({ route }: Props) => {
+const Programm = ({ route, navigation }: Props) => {
   const programmID = route.params.programmID;
   const store = useStore();
   let programm = store.getProgramm(programmID);
@@ -30,21 +30,48 @@ const Programm = ({ route }: Props) => {
         <Text>Программа не найдена</Text>
       </Container>
     );
+
+  const renderItem: ListRenderItem<TrainingDataProps> = ({ item, index }) => (
+    <TouchableRipple
+      style={globalStyle.padding10}
+      onPress={() => {
+        navigation.navigate("ProgrammsHome", {
+          screen: "Training",
+          params: { programmID, trainingID: item.id },
+        });
+      }}
+    >
+      <Text>{1 + index + ". " + item.name}</Text>
+    </TouchableRipple>
+  );
+
   return (
-    <Container style={globalStyle.container}>
-      <ExerciseThumb thumbImg={programm.thumbImg} />
-      <Text>{programm.name}</Text>
-      <Text>{programm.description}</Text>
-      <FlatList
-        data={programm.trainings}
-        renderItem={({ item, index }) => (
-          <Text>{index + ". " + item.name}</Text>
-        )}
-      />
-    </Container>
+    <View style={globalStyle.container}>
+      <BackgroundImageSurface img={programm.thumbImg}>
+        <Text variant="headlineMedium" style={styles.title}>
+          {programm.name}
+        </Text>
+      </BackgroundImageSurface>
+      <Container>
+        <Text>Описание:</Text>
+        <Text>{programm.description}</Text>
+      </Container>
+
+      <Surface style={globalStyle.mt20}>
+        <FlatList
+          data={programm.trainings}
+          renderItem={renderItem}
+          ItemSeparatorComponent={Divider}
+        />
+      </Surface>
+    </View>
   );
 };
 
 export default Programm;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  title: {
+    margin: 10,
+  },
+});
